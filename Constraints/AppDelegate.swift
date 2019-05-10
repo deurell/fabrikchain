2938c971f5de02cf6d71d8b70156e1c7ab62885e
mikael.deurell <mikael@deurell.net>
2019-05-10 10:41:15 +0200

    Initial Commit

---
+++ Constraints/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Constraints
+//
+//  Created by Mikael Deurell on 2019-05-10.
+//  Copyright © 2019 Mikael Deurell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
